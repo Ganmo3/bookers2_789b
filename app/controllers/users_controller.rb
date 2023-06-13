cbae42bfb080170b34b0e59bd097cb6e1c2df734
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-06-13 06:06:13 +0000

    [finish]7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update, :edit]
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+
+    # 投稿数
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
